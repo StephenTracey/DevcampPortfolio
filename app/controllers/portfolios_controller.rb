0e1ec7c7f4a90e21ad307ceb16d6d7ca39a5fc6f
Stevo <stephen.tracey88@gmail.com>
2019-06-02 00:19:56 +1000

    Installed devise gem

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,12 +8,14 @@ class PortfoliosController < ApplicationController
   end  
     
   
-  def new
+    def new
     @portfolio_item = Portfolio.new
-  end  
+    3.times { @portfolio_item.technologies.build }
+  end 
 
-  def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+
 
     respond_to do |format|
       if @portfolio_item.save
@@ -35,7 +37,7 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to portfolios_path, notice: 'The recordwas successfully updated.' }
+        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
        
       else
         format.html { render :edit }
